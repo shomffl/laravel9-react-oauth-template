c4783c0fed0b018a43b6cf04b01d0f6bb0e9b2ab
sho watanabe <showatanabe0226@gmail.com>
2022-09-02 23:36:00 +0900

    [udpate]DBをOAuth用に修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->bigInteger("github_id")->unsigned();
+            $table->string("github_token")->nullable();
+            $table->string("github_refresh_token")->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
