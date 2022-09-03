bcbc7e3c75e9d9a51bc57967a40e705928f9f6b0
sho watanabe <showatanabe0226@gmail.com>
2022-09-04 02:18:33 +0900

    [update]レイアウトの修正

--- resources/js/Pages/Auth/OAuthLogin.tsx
+++ resources/js/Pages/Auth/OAuthLogin.tsx
@@ -6,14 +6,14 @@ const OAuthLogin = () => {
         <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             <a
                 href="/auth/github/redirect"
-                className="bg-black text-white text-xl font-bold flex justify-center items-center py-2 rounded-lg shadow-lg hover:scale-105 duration-200 hover:shadow-xl"
+                className="bg-black text-white text-xl font-bold flex justify-center items-center py-2 rounded-lg shadow-lg hover:scale-105 duration-200 hover:shadow-xl active:scale-100"
             >
                 <img className="pr-3 h-8" src={"/icon/github.png"} />
                 <div>Sign in with GitHub</div>
             </a>
             <a
                 href="/auth/google/redirect"
-                className="bg-white border border-gray-300 text-xl font-bold flex justify-center items-center py-2 my-6 rounded-lg shadow-lg hover:scale-105 duration-200 hover:shadow-xl"
+                className="bg-white border border-gray-300 text-xl font-bold flex justify-center items-center py-2 my-6 rounded-lg shadow-lg hover:scale-105 duration-200 hover:shadow-xl active:scale-100"
             >
                 <div>
                     Sign in with <span className="text-blue-600">G</span>
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -18,7 +18,8 @@ export default function Dashboard(props: any) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
+                            You're logged in with&nbsp;
+                            {props.auth.user.oauth.provider.name}&nbsp;!
                         </div>
                     </div>
                 </div>
