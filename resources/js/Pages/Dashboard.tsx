522f9983b3e6fef04840c9f51aee37ae84ec8449
sho watanabe <showatanabe0226@gmail.com>
2022-09-04 02:41:02 +0900

    [fix]通常ログイン時にoauthのプロパティを読み込もうとしていたため修正

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -19,7 +19,8 @@ export default function Dashboard(props: any) {
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
                             You're logged in with&nbsp;
-                            {props.auth.user.oauth.provider.name}&nbsp;!
+                            {props.auth.user.oauth?.provider.name || "email"}
+                            &nbsp;!
                         </div>
                     </div>
                 </div>
