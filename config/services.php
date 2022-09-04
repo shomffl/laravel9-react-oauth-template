fcfdfb63d7aee5eab71ac0c609967a28652be75c
sho watanabe <showatanabe0226@gmail.com>
2022-09-04 11:30:40 +0900

    [update]callbackのURLをアプリケーションURLで管理できるように修正

--- config/services.php
+++ config/services.php
@@ -34,13 +34,13 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => env('GITHUB_REDIRECT_URL'),
+        'redirect' => env("APP_URL") + "/auth/1/callback",
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-	    'redirect' => env('GOOGLE_REDIRECT_URL'),
+	    'redirect' => env("APP_URL") + "/auth/2/callback"
     ],
 
 ];
